ae9bafe4e328199136fe361f3012741d25c831ee
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2026-03-12 05:53:18 +1000

    Updated system

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "QuoteMate",
     slug: "quotemate",
-    version: "1.0.63",
+    version: "1.0.64",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
@@ -80,7 +80,7 @@ export default {
         backgroundColor: "#1E293B"  // Dark blue-gray to match app theme
       },
       package: "com.quotemate.app",
-      versionCode: 73,
+      versionCode: 74,
       permissions: ["android.permission.RECORD_AUDIO"],
       blockedPermissions: ["android.permission.CAMERA"]
     },
