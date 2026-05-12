a3b40f6015ee0b6f1a64c1f620378f0d0777b4c0
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2026-05-13 05:21:12 +1000

    Xero don

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ export default {
   expo: {
     name: "QuoteMate",
     slug: "quotemate",
-    version: "1.33",
+    version: "1.34",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
