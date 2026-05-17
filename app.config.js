edea6dd326d8582ffaa17b1520249a3fbced60d2
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2026-05-17 11:55:54 +1000

    Major Refactor

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ export default {
   expo: {
     name: "QuoteMate",
     slug: "quotemate",
-    version: "1.37",
+    version: "1.38",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
