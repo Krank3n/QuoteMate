fdfb4681e898cc9152f9726a815a0143c2a550bb
Thomas A Hansen <45204791+Krank3n@users.noreply.github.com>
2026-05-19 15:12:21 +1000

    Fix welcome / onboarding email

--- app.config.js
+++ app.config.js
@@ -9,7 +9,7 @@ export default {
   expo: {
     name: "QuoteMate",
     slug: "quotemate",
-    version: "1.39",
+    version: "1.40",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
